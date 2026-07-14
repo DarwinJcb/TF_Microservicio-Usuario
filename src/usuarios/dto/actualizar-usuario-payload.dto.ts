e65c1eba381973b0f4373a81796400737f59e2a7
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-14 11:44:47 -0500

    Commit #41

--- src/usuarios/dto/actualizar-usuario-payload.dto.ts
+++ src/usuarios/dto/actualizar-usuario-payload.dto.ts
@@ -1,10 +1,11 @@
 /* src/usuarios/dto/actualizar-usuario-payload.dto.ts */
 import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { IsDefined, ValidateNested } from 'class-validator';
 import { IdUsuarioDto } from './id-usuario.dto';
 import { UpdateUsuarioDto } from './update-usuario.dto';
 
 export class ActualizarUsuarioPayloadDto extends IdUsuarioDto {
+  @IsDefined()
   @ValidateNested()
   @Type(() => UpdateUsuarioDto)
   datosUsuario: UpdateUsuarioDto;
